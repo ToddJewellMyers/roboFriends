d455db91100586ca6cd4feae2dcff1f064391153
Todd Myers <todd1988myers@gmail.com>
2022-11-13 07:23:27 -0500

    Updates

---
+++ precache-manifest.e8d628e4a21f7b83f2261b09b4a9826f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38e59c3b87f4a01ca046",
+    "url": "/robofriends/static/css/main.7867dc13.chunk.css"
+  },
+  {
+    "revision": "38e59c3b87f4a01ca046",
+    "url": "/robofriends/static/js/main.38e59c3b.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "6be527b74dd6a23d793a",
+    "url": "/robofriends/static/css/2.25c78a57.chunk.css"
+  },
+  {
+    "revision": "6be527b74dd6a23d793a",
+    "url": "/robofriends/static/js/2.6be527b7.chunk.js"
+  },
+  {
+    "revision": "6bf665704170461feaf8",
+    "url": "/robofriends/static/js/runtime~main.6bf66570.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9e6be3aa149585df79cbbf05cf445509",
+    "url": "/robofriends/index.html"
+  }
+];
